0a2a7cf0614b174a963c15b9eaaec1c5eda44b20
Amber Jones <amberlovescats14@gmail.com>
2019-11-14 16:24:57 -0600

    module exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//? item and importing are examples
+
 // const $ = require('jquery')
 // const {item, thing} = require('./item.js')
 import $ from 'jquery'
@@ -6,9 +8,11 @@ const {item, thing} = require('./item')
 import {cami, mason} from './importing'
 import {cami as daughter, mason as son} from './importing'
 
+import { sayHello } from './say-hello'
+
 $(document).ready(function () {
-    const sayHello = () => console.log(`Hello`)
-    sayHello()
+    // const sayHello = () => console.log(`Hello`)
+    // sayHello()
     $('body').append(`<h1>Hello World</h1>`).css({
         background: 'black',
         color: 'pink'
@@ -19,6 +23,7 @@ $(document).ready(function () {
     
     console.log(son.gender)
     
+    console.log(sayHello('Amber'))
     
     
 })
---
+++ src/item.js
@@ -0,0 +1,14 @@
+const item = {
+    name: 'amber',
+    age: 32
+}
+
+const thing = {
+    name: 'milk',
+    color: 'white'
+}
+
+module.exports = {
+    item,
+    thing
+}
